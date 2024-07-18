51c1df74ea0ce32e56effb5d3429ec1a804c0132
Arnav <arnav2001bhattarai@gmail.com>
2024-07-18 17:35:32 +0545

    learned  about variables

---
+++ variables.js
@@ -0,0 +1,27 @@
+// ES5
+var person = "Arnav"; //(global variable)
+
+// ES6
+const secondPerson = "Bibek";
+let thirdPerson = "Ram";
+
+console.log(person, secondPerson, thirdPerson);
+
+var test = "Arnav";
+var test = "Avi";
+console.log(test);
+
+const testPerson = "raktim"; //(fixed variable name declare)
+console.log(testPerson);
+
+let newPerson = "abc"; //(variable update garna dido raixa)
+newPerson = "mannu";
+
+console.log(newPerson);
+
+// RULE=Always start VARIABLE with  const keyword because you can change it to let later
+
+const table = "red table";
+
+let phone = 123456789;
+phone = 789456123;
