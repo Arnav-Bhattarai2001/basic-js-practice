b654bfaa7a38ecaee9edd0869946a95d00c7be52
Arnav <arnav2001bhattarai@gmail.com>
2024-07-18 17:36:05 +0545

    learned about comments

---
+++ comments.js
@@ -0,0 +1,15 @@
+//Single line comment
+
+// This is a sing line comment
+
+//Multi line comment (API DOCUMENTATION or LOGIC EXPLAIN)
+
+/*
+dsbfbkjf
+akdgj
+dakgjknsfkjg
+adfjkg
+asldjbgfkjbg
+*/
+
+// CTRL + / (after selecting all lines)
