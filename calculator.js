f8275df09d05a5f72ed13dc9bdc321a546dca54f
Arnav <arnav2001bhattarai@gmail.com>
2024-07-19 17:05:21 +0545

    learned datatypes

---
+++ calculator.js
@@ -0,0 +1,2 @@
+type coercion / conversion
+
---
+++ data-types.js
@@ -0,0 +1,19 @@
+const a = 2;
+console.log(a);
+
+const b = Number(a);
+console.log(b);
+
+const c = String(b);
+console.log(c);
+
+
+
+// Datatype(5 types)
+// 1.Primitive Datatypes
+// 2. Complex
+// type conversion / coercion
+// const a = "2";
+// console.log(typeof a); //check type
+// const num= Number(a);  //type conversion
+// console.log(num);
