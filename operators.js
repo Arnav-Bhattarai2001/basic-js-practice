a17bac281d917fff9f0457320629f4bb45e0af1e
Arnav <arnav2001bhattarai@gmail.com>
2024-07-19 18:05:10 +0545

    learned operands

---
+++ operators.js
@@ -0,0 +1,22 @@
+// Operators
+//
+
+
+
+1. AND (both condition must be true)
+condition1 && condition2
+// true && true
+
+2. OR (any one must be true)
+condition1 || condition2
+
+3. not (opposite value)
+
+const isAdult = true;
+console.log(!isAdult);
+
+const isAdult = false;
+
+if (!isAdult) {
+    console.log()
+}
