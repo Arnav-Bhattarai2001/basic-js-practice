379bad96b0b49317fecc4901676d6e1d28500357
Arnav <arnav2001bhattarai@gmail.com>
2024-07-19 17:27:29 +0545

    learned if else

---
+++ statement.js
@@ -0,0 +1,37 @@
+// Statements
+// 1. if else
+// 2. Switch case
+
+//1. if else
+// SYNTAX:
+// if (condition){
+//     LOGIC
+// }
+// else {
+//     LOGIC
+// }
+
+// const gender = confirm("Are you male?")
+// if (gender) {
+//     alert("You are male"); //if true
+// } else {
+//     alert{"You are female"};
+// }
+
+// const gender = prompt("Enter M for male and F for female:");
+// if (gender === "M") {
+//   alert("You are male");
+// } else {
+//   alert("You are female");
+// }
+
+
+// =      Variable assign
+// ==     data type and value compare
+// ===    data type and value compare + data conversion to boolean  (always use for value comparisions)
+
+
+// 2. Switch case
+
+
+
