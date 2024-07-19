fbf3039627b7cd3e08bb1bfd350b73e9f317e2ca
Arnav <arnav2001bhattarai@gmail.com>
2024-07-19 17:55:33 +0545

    learned conditional

--- statement.js
+++ statement.js
@@ -25,13 +25,57 @@
 //   alert("You are female");
 // }
 
-
 // =      Variable assign
 // ==     data type and value compare
 // ===    data type and value compare + data conversion to boolean  (always use for value comparisions)
 
-
 // 2. Switch case
 
+// CHECK MULTIPLE CASES
+
+// SYNTAX: switch (condition) {
+//   case "1":
+//     alert("sunday");
+//     break;
+//   case "2":
+//     alert("Monday");
+//     break;
+//   default:
+//     alert("You have to choose betn 1 and 7");
+// }
+
+// WRITE A JS PROGRAM TO CHECK NAME OF DAYS
+
+// WRITE A JS PROGRAM TO CHECK GRADE OF STUDENT
+
+// const score = Number(prompt("Enter your percentage?"));
+
+// switch (true) {
+//   case score >= 100:
+//     alert("You have scored GRADE A ");
+//     break;
+//   case score >= 60 && score <80:
+//     alert("You have scored GRADE B ");
+//     break;
+//   default:
+//     alert("You have failed your exam.");
+// }
+
+// CONDITIONAL OPERATOR (TERNARY OPERATOR)
 
 
+
+// IF (){} ELSE{} ES5
+// TERNARY OPERATOR  ES6
+
+
+// const gender = "m";
+// if (gender === "m") {
+//   console.log("m");
+// } else {
+//   console.log("f");
+
+
+// SYNTAX:
+// (condition) ? true : false;
+gender === "m" ? console.log("m") : console.log("f"); //m
