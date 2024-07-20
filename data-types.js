95b097a31a5555ccee2c7f93c10d524605735f0c
Arnav <arnav2001bhattarai@gmail.com>
2024-07-20 18:40:54 +0545

    learned datatype conversion

--- data-types.js
+++ data-types.js
@@ -1,14 +1,3 @@
-const a = 2;
-console.log(a);
-
-const b = Number(a);
-console.log(b);
-
-const c = String(b);
-console.log(c);
-
-
-
 // Datatype(5 types)
 // 1.Primitive Datatypes
 // 2. Complex
@@ -17,3 +6,22 @@ console.log(c);
 // console.log(typeof a); //check type
 // const num= Number(a);  //type conversion
 // console.log(num);
+
+// const a = 2;
+// console.log(a);
+
+// const b = Number(a);
+// console.log(b);
+
+// const c = String(b);
+// console.log(c);
+
+const a = "2"; // If declared within double inverted comma, then it is regarded as "String" by JavaScript
+console.log(typeof a);
+
+const b = 2;
+console.log(typeof b); //If not declared within double inverted comma, then it is regarded as "Number" by JavaScript
+
+const c = Number(a); //Type conversion from "string" to "number"
+console.log(c);
+console.log(typeof c);
