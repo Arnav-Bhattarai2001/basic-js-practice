e559be8bacd8be3ee56bb65029de093713e147be
Arnav <arnav2001bhattarai@gmail.com>
2024-07-24 17:57:17 +0545

    learned new question

---
+++ objectsExercise.js
@@ -0,0 +1,17 @@
+// a. Find the value of the price property and if it is greater than 100, discount it by 10%. If that’s not the case, discount it by 7%. 
+// b. Update the object with the new property - discount and the corresponding value (7% or 10%) and the new price.
+
+let prod = {
+    name: "headphones",
+    price: 83.7,
+    discount: "7%",
+    };
+
+const 
+
+const newObject = (prod) => {
+    if (prod.price < 100){
+        prod
+    }
+
+}
