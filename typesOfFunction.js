b435da27b0ce3d32b9bf5efdeabc638e7e4d9ce2
Arnav <arnav2001bhattarai@gmail.com>
2024-07-24 22:42:56 +0545

    completed function types

--- typesOfFunction.js
+++ typesOfFunction.js
@@ -1,148 +1,146 @@
-// // TYPES OF FUNCTION :
+// TYPES OF FUNCTION :
 
-// // 1. DEFAULT Function : Used for pagination  
+// 1. DEFAULT Function : Used for pagination
 
+const defaultFunc = (name) => {
+  console.log(`Hello ${name}`);
+};
+defaultFunc("Arnav"); // Hello Arnav
+defaultFunc("Admin"); // Hello Admin
+defaultFunc(); // Hello undefined
 
-// const defaultFunc = (name) => {
-//   console.log(`Hello ${name}`);
+const defaultFunc1 = (name = "User") => {
+  console.log(`Hello ${name}`);
+};
+defaultFunc1("Arnav");
+defaultFunc1("Admin");
+defaultFunc1(); // Hello User
+
+// 2. Parameterized Function :
+
+const sumation = (a, b, c, d) => {
+  console.log(a + b);
+};
+sumation(2, 3, 1, 4);
+
+// Parameterized function applied :
+const sumations = ({ a, b, c, d }) => {
+  console.log(a + b + c + d);
+};
+sumations({ a: 2, b: 3, c: 1, d: 4 });
+
+// 3. Arrow Function :
+
+const summ = (a, b) => {
+  return a + b;
+};
+
+// 4. closure Function :
+
+// FOR EXPLANATION:
+// let count = 0;
+// const counter = () => {
+//   count = count + 1;
+//   return count;
 // };
-// defaultFunc("Arnav"); // Hello Arnav
-// defaultFunc("Admin"); // Hello Admin
-// defaultFunc(); // Hello undefined
+// console.log(counter());
+// console.log(counter());
+// console.log(counter());
 
-// const defaultFunc1 = (name = "User") => {
-//   console.log(`Hello ${name}`);
-// };
-// defaultFunc1("Arnav");
-// defaultFunc1("Admin");
-// defaultFunc1(); // Hello User
+// ACTUAL COUNTER FUNCTION:
+const mainCounter = () => {
+  let count = 0;
+  return () => {
+    count = count + 1;
+    return count;
+  };
+};
+const count1 = mainCounter(); // dashain counter
+const count2 = mainCounter(); // tihar counter
+console.log(count1());
+console.log(count1());
+console.log(count1());
 
-// // 2. Parameterized Function :
+console.log(count2());
+console.log(count2());
 
-// const sumation = (a, b, c, d) => {
-//   console.log(a + b);
-// };
-// sumation(2, 3, 1, 4);
+// 6. Recursive function
 
-// // Parameterized function applied :
-// const sumations = ({ a, b, c, d }) => {
-//   console.log(a + b + c + d);
-// };
-// sumations({ a: 2, b: 3, c: 1, d: 4 });
+// factorial of 5
 
-// // 3. Arrow Function :
+const factorial = (num) => {
+  if (num === 0 || num === 1) {
+    return 1;
+  }
+  return num * factorial(num - 1);
+};
 
-// const summ = (a, b) => {
-//   return a + b;
-// };
+const fact = factorial(5);
+console.log(fact);
 
-// // 4. closure Function :
+// write a recursion formula for countdown
 
-// // FOR EXPLANATION:
-// // let count = 0;
-// // const counter = () => {
-// //   count = count + 1;
-// //   return count;
-// // };
-// // console.log(counter());
-// // console.log(counter());
-// // console.log(counter());
+const countDown = (counter) => {
+  console.log(counter);
+  let count = counter - 1;
+  if (count === 0) {
+    return 0;
+  }
+  return countDown(count);
+};
+countDown(10);
 
-// // ACTUAL COUNTER FUNCTION:
-// const mainCounter = () => {
-//   let count = 0;
-//   return () => {
-//     count = count + 1;
-//     return count;
-//   };
-// };
-// const count1 = mainCounter(); // dashain counter
-// const count2 = mainCounter(); // tihar counter
-// console.log(count1());
-// console.log(count1());
-// console.log(count1());
+// 7. Anonymous function
 
-// console.log(count2());
-// console.log(count2());
+(a) => {
+  console.log(a);
+};
 
-// // 6. Recursive function
+// 8. Inline Function :
 
-// // factorial of 5
+// const test = () => {}; // We get Inline Function if we assign Anonymous Function to a Variable.
 
-// const factorial = (num) => {
-//   if (num === 0 || num === 1) {
-//     return 1;
-//   }
-//   return num * factorial(num - 1);
-// };
+const test = () => {
+  const d = 1;
+  console.log({ d });
+};
+test();
 
-// const fact = factorial(5);
-// console.log(fact);
+// ES6 :
+const tests = () => {
+  const dd = 1;
+  console.log({ d });
+};
+const dd = "alpha"; //  The variable dd(inside function) didnt get affected the declaration of same variable outside the function.
+console.log({ d });
+tests();
 
-// // write a recursion formula for countdown
+// ES5 :
+const d = "alpha";
+function testss() {
+  console.log(`global variable : ${d}`);
+}
+testss();
 
-// const countDown = (counter) => {
-//   console.log(counter);
-//   let count = counter - 1;
-//   if (count === 0) {
-//     return 0;
-//   }
-//   return countDown(count);
-// };
-// countDown(10);
+// Output : [global variable : alpha]
+// bahira declare gareko variable "d" ko value bhitra function le liyo , So in ES5 function self contained hudaina
 
-// // 7. Anonymous function
+// 9. CallBack Function :
 
-// (a) => {
-//   console.log(a);
-// };
+const print = (data) => {
+  console.log({ data });
+};
 
-// // 8. Inline Function :
+const parent = (abcde) => {
+  const num = "Arnav";
+  abcde(num);
+};
 
-// // const test = () => {}; // We get Inline Function if we assign Anonymous Function to a Variable.
+parent(print);
 
-// const test = () => {
-//   const d = 1;
-//   console.log({ d });
-// };
-// test();
-
-// // ES6 :
-// const tests = () => {
-//   const dd = 1;
-//   console.log({ d });
-// };
-// const dd = "alpha"; //  The variable dd(inside function) didnt get affected the declaration of same variable outside the function.
-// console.log({ d });
-// tests();
-
-// // ES5 :
-// const d = "alpha";
-// function testss() {
-//   console.log(`global variable : ${d}`);
-// }
-// testss();
-
-// // Output : [global variable : alpha]
-// // bahira declare gareko variable "d" ko value bhitra function le liyo , So in ES5 function self contained hudaina
-
-// // 9. CallBack Function :
-
-// const print = (data) => {
-//   console.log({ data });
-// };
-
-// const parent = (abcde) => {
-//   const num = "Arnav";
-//   abcde(num);
-// };
-
-// parent(print);
-
-// 10. Pure Function : 
+// 10. Pure Function :
 
 // In JavaScript, a pure function is a function that always produces the same output for the same input
-
 
 const upperConverter = (text) => {
   console.log(text.toUpperCase());
@@ -154,4 +152,15 @@ upperConverter("Bhattarai");
 
 // 11. Implicit Function :
 
+const sum = (a, b) => {
+  return a + b;
+};
+
+const sums = (a, b) => a + b;
+console.log(sums(2, 4));
+
 // 12. Explicit Function :
+
+const sum = (a, b) => {
+  return a + b;
+};
