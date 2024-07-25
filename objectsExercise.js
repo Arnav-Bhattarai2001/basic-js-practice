93e76bb73fd9e60a6d249dbc297ea2e15a164f36
Arnav <arnav2001bhattarai@gmail.com>
2024-07-25 15:30:12 +0545

    did product question

--- objectsExercise.js
+++ objectsExercise.js
@@ -1,17 +1,27 @@
-// a. Find the value of the price property and if it is greater than 100, discount it by 10%. If that’s not the case, discount it by 7%. 
+// a. Find the value of the price property and if it is greater than 100, discount it by 10%. If that’s not the case, discount it by 7%.
 // b. Update the object with the new property - discount and the corresponding value (7% or 10%) and the new price.
 
 let prod = {
-    name: "headphones",
-    price: 83.7,
-    discount: "7%",
-    };
+  name: "headphones",
+  price: 83.7,
+  discount: "7%",
+};
 
-const 
+const newProd = prod;
 
-const newObject = (prod) => {
-    if (prod.price < 100){
-        prod
-    }
+const newObject = (newProd) => {
+  if (prod.price > 100) {
+    prod.discount = "10%";
+    prod.price = `${price} - (0.10 * ${price})`;
+  }
+  return newProd;
+};
 
-}
+const newObject1 = (newProd) => {
+  if (prod.price <= 100) {
+    prod.price = `${price} - (0.07 * ${price})`;
+  }
+  return newProd;
+};
+
+console.log({ newProd });
