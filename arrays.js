5b9b82553d2042288928a6d0ff0f46a9ba25c6dd
Arnav <arnav2001bhattarai@gmail.com>
2024-07-25 17:19:51 +0545

    learned arrays

---
+++ arrays.js
@@ -0,0 +1,69 @@
+// ARRAYS
+// Collection of items
+// CRUD
+
+// CREATE
+
+// const arr = new Array();
+// console.log({ arr });
+
+// const arrV1 = [];
+
+const cars = [
+  { name: "Tesla", model: 2012 },
+  { name: "Nexon", model: 2023 },
+  { name: "i10", model: 2011 },
+  { name: "Scorpio", model: 2017 },
+];
+
+// Read
+
+console.log(cars[0]);
+
+// Update
+
+// const newName = "Tesla Model X";
+// cars[0].name = newName;
+
+// const copiesOfCars = Cars; // referential value [don't use]
+// const copyOfCars = cars;
+// console.log({ copyOfCars });
+// const newName = "Tesla Model X";
+// cars[0].name = newName;
+
+// console.log({ copyOfCars });
+
+let copyOfCars = cars.slice();
+let copyOfCars = [...cars]; //[{},{},{},{}]
+
+const newName = "Tesla Model X";
+copyOfCars[0].name = newName;
+
+console.log({ copyOfCars });
+
+// delete
+
+const [a, ...rest] = copyOfCars;
+console.log({ rest });
+
+//Array operations (methods)
+// mozilla
+
+// cars.length();
+// cars.sort();
+// cars.push();
+// cars.pop();
+
+// cars.slice(2);
+// cars.slice(2,4);
+// cars.slice(start, end(end not included));
+
+Map;
+Filter;
+reduce;
+sluce;
+splice;
+some;
+every;
+includes;
+find;
