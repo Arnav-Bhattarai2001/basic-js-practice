b588bcd2371364af75992a34b7fbce5f69ce8fdd
Arnav <arnav2001bhattarai@gmail.com>
2024-07-27 20:25:20 +0545

    practiced

--- arraysPractice.js
+++ arraysPractice.js
@@ -1,32 +1,32 @@
-// // Use of array methods
+// Use of array methods
 
-// // write a js function that sorts the age in ascending order
+// write a js function that sorts the age in ascending order
 
-// const people = [
-//   { name: "raktim", age: 30 },
-//   { name: "anjali", age: 18 },
-//   { name: "paras", age: 25 },
-// ];
-
-// // const ageSorter = (arr) => {
-// //   // process the value
-// //   return arr.sort((a, b) => {
-// //     // Callback function
-// //     a.age - b.age;
-// //   });
-// // };
+const people = [
+  { name: "raktim", age: 30 },
+  { name: "anjali", age: 18 },
+  { name: "paras", age: 25 },
+];
 
 // const ageSorter = (arr) => {
-//   const compareFn = (a, b) => a.age - b.age;
-//   return arr.sort(compareFn);
+//   // process the value
+//   return arr.sort((a, b) => {
+//     // Callback function
+//     a.age - b.age;
+//   });
 // };
 
-// // Or convert into implicit function for better understanding
-// // const ageSorter = (arr) => {
-// //   return arr.sort((a, b) => a.age - b.age);
-// // };
+const ageSorter = (arr) => {
+  const compareFn = (a, b) => a.age - b.age;
+  return arr.sort(compareFn);
+};
 
-// const result = ageSorter(people);
-// console.log({ result });
+// Or convert into implicit function for better understanding
+// const ageSorter = (arr) => {
+//   return arr.sort((a, b) => a.age - b.age);
+// };
+
+const result = ageSorter(people);
+console.log({ result });
 
 
