11c21c2418f988e155d8a51647d061b90ce561e9
Arnav <arnav2001bhattarai@gmail.com>
2024-09-14 13:48:14 +0545

    practiced questions

--- pi.js
+++
@@ -1,9 +0,0 @@
-// Write a JavaScript program to set the value of PI.
-
-// const value = confirm("DO YOU WANT TO KNOW THE VALUE OF PI?");
-// value === true
-//   ? alert("The value of PI is 3.14")
-//   : alert("Try something else.");
-
-const valueOfPi = Number(prompt("ENTER THE VALUE OF PI:"));
-alert(`The value of PI set by user is : ${valueOfPi}`);
---
+++ practiceForme.js
@@ -0,0 +1,28 @@
+IMPORTANT QUESTIONS : 
+
+// 1. let food = ['Noodle', 'Pasta', 'Ice-cream'];
+// let food = ['Fries', 'Ice-cream', 'Pizza'];
+// Compare the 2 arrays and find common food if any.
+
+// const foodA = ["Noodle", "Pasta", "Ice-cream"];
+// const foodB = ["Fries", "Ice-cream", "Pizza"];
+
+// const findItem = (a, b) => a.find((item) => b.includes(item));
+
+// console.log(findItem(foodA, foodB));
+
+
+//2. eg : arnav bhattarai => Arnav bhattarai
+
+// const properCase = (text) => {
+//   return text
+//     .split(" ")
+//     .map(
+//       (word) =>
+//         word.slice(0, 1).toUpperCase() +
+//         word.slice(1, word.length).toLowerCase()
+//     )
+//     .join(" ");
+// };
+
+// console.log(properCase("arNav bhaTtarai"));
