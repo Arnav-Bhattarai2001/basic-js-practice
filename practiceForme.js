38853aafe84817ab03748c6ef61ea09b5d20b644
Arnav <arnav2001bhattarai@gmail.com>
2024-09-14 13:50:49 +0545

    question 3 completed

--- practiceForme.js
+++ practiceForme.js
@@ -1,4 +1,4 @@
-IMPORTANT QUESTIONS : 
+// IMPORTANT QUESTIONS : 
 
 // 1. let food = ['Noodle', 'Pasta', 'Ice-cream'];
 // let food = ['Fries', 'Ice-cream', 'Pizza'];
@@ -26,3 +26,35 @@ IMPORTANT QUESTIONS :
 // };
 
 // console.log(properCase("arNav bhaTtarai"));
+
+// 3. Get total mass of all characters using reduce method
+const characters = [
+    {
+        name: 'Luke Skywalker',
+        height: '172',
+        mass: '77',
+        eye_color: 'blue',
+        gender: 'male',
+    },
+    {
+        name: 'Darth Vader',
+        height: '202',
+        mass: '136',
+        eye_color: 'yellow',
+        gender: 'male',
+    },
+    {
+        name: 'Leia Organa',
+        height: '150',
+        mass: '49',
+        eye_color: 'brown',
+        gender: 'female',
+    },
+    {
+        name: 'Anakin Skywalker',
+        height: '188',
+        mass: '84',
+        eye_color: 'blue',
+        gender: 'male',
+    },
+];
