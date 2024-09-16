5a1c8f390238dd621b00c1d97bff3571d0933be9
Arnav <arnav2001bhattarai@gmail.com>
2024-09-16 21:41:42 +0545

    practiced js

--- arrayGithubQuestion.js
+++ arrayGithubQuestion.js
@@ -1,141 +1,141 @@
-// Question 1 : SORT
-// a. Sort by name
-// b. Sort by mass
-// c. Sort by height
-// d. Sort by gender
+// // Question 1 : SORT
+// // a. Sort by name
+// // b. Sort by mass
+// // c. Sort by height
+// // d. Sort by gender
 
-const characters = [
-  {
-    name: "Luke Skywalker",
-    height: "172",
-    mass: "77",
-    eye_color: "blue",
-    gender: "male",
-  },
-  {
-    name: "Darth Vader",
-    height: "202",
-    mass: "136",
-    eye_color: "yellow",
-    gender: "male",
-  },
-  {
-    name: "Leia Organa",
-    height: "150",
-    mass: "49",
-    eye_color: "brown",
-    gender: "female",
-  },
-  {
-    name: "Anakin Skywalker",
-    height: "188",
-    mass: "84",
-    eye_color: "blue",
-    gender: "male",
-  },
-];
+// const characters = [
+//   {
+//     name: "Luke Skywalker",
+//     height: "172",
+//     mass: "77",
+//     eye_color: "blue",
+//     gender: "male",
+//   },
+//   {
+//     name: "Darth Vader",
+//     height: "202",
+//     mass: "136",
+//     eye_color: "yellow",
+//     gender: "male",
+//   },
+//   {
+//     name: "Leia Organa",
+//     height: "150",
+//     mass: "49",
+//     eye_color: "brown",
+//     gender: "female",
+//   },
+//   {
+//     name: "Anakin Skywalker",
+//     height: "188",
+//     mass: "84",
+//     eye_color: "blue",
+//     gender: "male",
+//   },
+// ];
 
-// a. Sort by name
+// // a. Sort by name
 
-const sortName = [...characters];
+// const sortName = [...characters];
 
-const nameSorter = (a, b) => {
-  const nameA = a.name.toLowerCase();
-  const nameB = b.name.toLowerCase();
-  if (nameA < nameB) {
-    return -1; // a comes before b
-  }
-  if (nameB > nameA) {
-    return 1; // a comes after b
-  }
-  return 0; // names are equal
-};
-const sortNameResult = sortName.sort(nameSorter);
-console.log(sortNameResult);
+// const nameSorter = (a, b) => {
+//   const nameA = a.name.toLowerCase();
+//   const nameB = b.name.toLowerCase();
+//   if (nameA < nameB) {
+//     return -1; // a comes before b
+//   }
+//   if (nameB > nameA) {
+//     return 1; // a comes after b
+//   }
+//   return 0; // names are equal
+// };
+// const sortNameResult = sortName.sort(nameSorter);
+// console.log(sortNameResult);
 
-/*
-A comparison function is used to determine the order of elements. It takes two arguments, typically two elements from the array, and returns:
+// /*
+// A comparison function is used to determine the order of elements. It takes two arguments, typically two elements from the array, and returns:
 
-a. (nameA < nameB) -1 == if the first element should come before the second element.
-b. (nameB > nameA) 1 == if the first element should come after the second element.
-c. (nameB = nameA) 0 == if the elements are equal and their order doesn't matter.
-*/
+// a. (nameA < nameB) -1 == if the first element should come before the second element.
+// b. (nameB > nameA) 1 == if the first element should come after the second element.
+// c. (nameB = nameA) 0 == if the elements are equal and their order doesn't matter.
+// */
 
-// b. Sort by mass
+// // b. Sort by mass
 
-const sortMass = [...characters];
+// const sortMass = [...characters];
 
-const massSorter = (mass) => {
-  const massNumber = Number(sortMass.mass);
-  const compareFn = (a, b) => a.mass - b.mass;
-  return mass.sort(compareFn);
-};
-const sortMassResult = massSorter(sortMass);
-console.log(sortMassResult);
+// const massSorter = (mass) => {
+//   const massNumber = Number(sortMass.mass);
+//   const compareFn = (a, b) => a.mass - b.mass;
+//   return mass.sort(compareFn);
+// };
+// const sortMassResult = massSorter(sortMass);
+// console.log(sortMassResult);
 
-// c. Sort by height
+// // c. Sort by height
 
-const sortHeight = [...characters];
+// const sortHeight = [...characters];
 
-const heightSorter = (height) => {
-  const heightNumber = Number(sortHeight.height);
-  const compareFn = (a, b) => a.height - b.height;
-  return sortHeight.sort(compareFn);
-};
-const sortHeightResult = heightSorter(sortHeight);
-console.log(sortHeightResult);
+// const heightSorter = (height) => {
+//   const heightNumber = Number(sortHeight.height);
+//   const compareFn = (a, b) => a.height - b.height;
+//   return sortHeight.sort(compareFn);
+// };
+// const sortHeightResult = heightSorter(sortHeight);
+// console.log(sortHeightResult);
 
-// d. Sort by gender
+// // d. Sort by gender
 
-const sortGender = [...characters];
+// const sortGender = [...characters];
 
-const genderSorter = (a, b) => {
-  const genderA = a.gender.toLowerCase();
-  const genderB = b.gender.toLowerCase();
-  if (genderA < genderB) {
-    return -1;
-  }
-  if (genderA > genderB) {
-    return 1;
-  }
-  return 0;
-};
-const genderSortResult = sortGender.sort(genderSorter);
-console.log(genderSortResult);
+// const genderSorter = (a, b) => {
+//   const genderA = a.gender.toLowerCase();
+//   const genderB = b.gender.toLowerCase();
+//   if (genderA < genderB) {
+//     return -1;
+//   }
+//   if (genderA > genderB) {
+//     return 1;
+//   }
+//   return 0;
+// };
+// const genderSortResult = sortGender.sort(genderSorter);
+// console.log(genderSortResult);
 
-// Question 2 : MAP
+// // Question 2 : MAP
 
-// a . Get an array of all names
-// b . Get an array of all heights
-// c . Get an array of objects with just name and height properties
-// d . Get an array of all first names
+// // a . Get an array of all names
+// // b . Get an array of all heights
+// // c . Get an array of objects with just name and height properties
+// // d . Get an array of all first names
 
-// a . Get an array of all names
+// // a . Get an array of all names
 
-// const names = nameMap.map((a, b) => a + b);
-// const names = nameMap.map( a => a.name);
+// // const names = nameMap.map((a, b) => a + b);
+// // const names = nameMap.map( a => a.name);
 
-const nameMap = [...characters];
-const names = nameMap.map((nameMapped) => nameMapped.name);
-console.log(names);
+// const nameMap = [...characters];
+// const names = nameMap.map((nameMapped) => nameMapped.name);
+// console.log(names);
 
-// b . Get an array of all heights
+// // b . Get an array of all heights
 
-const heightMap = [...characters];
-const heights = heightMap.map((heightMapped) => heightMapped.height);
-console.log(heights);
+// const heightMap = [...characters];
+// const heights = heightMap.map((heightMapped) => heightMapped.height);
+// console.log(heights);
 
-// c . Get an array of objects with just name and height properties
+// // c . Get an array of objects with just name and height properties
 
-const nameHeightMap = [...characters];
-const heightAndNames = nameHeightMap.map((hn) => ({
-  name: hn.name,
-  height: hn.height,
-}));
-console.log(heightAndNames);
+// const nameHeightMap = [...characters];
+// const heightAndNames = nameHeightMap.map((hn) => ({
+//   name: hn.name,
+//   height: hn.height,
+// }));
+// console.log(heightAndNames);
 
-// d . Get an array of all first names
+// // d . Get an array of all first names
 
-const nameMaps = [...characters];
-const named = nameMaps.map((nameMapped) => nameMapped.name.split(" ")[0]);
-console.log(named);
+// const nameMaps = [...characters];
+// const named = nameMaps.map((nameMapped) => nameMapped.name.split(" ")[0]);
+// console.log(named);
--- practiceForme.js
+++ practiceForme.js
@@ -1,75 +1,75 @@
-// // IMPORTANT QUESTIONS :
+// IMPORTANT QUESTIONS :
 
-// // 1. let food = ['Noodle', 'Pasta', 'Ice-cream'];
-// // let food = ['Fries', 'Ice-cream', 'Pizza'];
-// // Compare the 2 arrays and find common food if any.
+// 1. let food = ['Noodle', 'Pasta', 'Ice-cream'];
+// let food = ['Fries', 'Ice-cream', 'Pizza'];
+// Compare the 2 arrays and find common food if any.
 
-// // const foodA = ["Noodle", "Pasta", "Ice-cream"];
-// // const foodB = ["Fries", "Ice-cream", "Pizza"];
+// const foodA = ["Noodle", "Pasta", "Ice-cream"];
+// const foodB = ["Fries", "Ice-cream", "Pizza"];
 
-// // const findItem = (a, b) => a.find((item) => b.includes(item));
+// const findItem = (a, b) => a.find((item) => b.includes(item));
 
-// // console.log(findItem(foodA, foodB));
+// console.log(findItem(foodA, foodB));
 
-// //2. eg : arnav bhattarai => Arnav bhattarai
+//2. eg : arnav bhattarai => Arnav bhattarai
 
-// // const properCase = (text) => {
-// //   return text
-// //     .split(" ")
-// //     .map(
-// //       (word) =>
-// //         word.slice(0, 1).toUpperCase() +
-// //         word.slice(1, word.length).toLowerCase()
-// //     )
-// //     .join(" ");
-// // };
-
-// // console.log(properCase("arNav bhaTtarai"));
-
-// // 3. Get values of all characters using reduce method
-// const characters = [
-//   {
-//     name: "Luke Skywalker",
-//     height: "172",
-//     mass: "77",
-//     eye_color: "blue",
-//     gender: "male",
-//   },
-//   {
-//     name: "Darth Vader",
-//     height: "202",
-//     mass: "136",
-//     eye_color: "yellow",
-//     gender: "male",
-//   },
-//   {
-//     name: "Leia Organa",
-//     height: "150",
-//     mass: "49",
-//     eye_color: "brown",
-//     gender: "female",
-//   },
-//   {
-//     name: "Anakin Skywalker",
-//     height: "188",
-//     mass: "84",
-//     eye_color: "blue",
-//     gender: "male",
-//   },
-// ];
-// // Get total mass of all characters using reduce method
-// const getMass = (arr) => {
-//   return arr.reduce((prev, current) => prev + Number(current.mass), 0); //for first iteration , prev is 0 and for next iterations, prev is previously added values.
+// const properCase = (text) => {
+//   return text
+//     .split(" ")
+//     .map(
+//       (word) =>
+//         word.slice(0, 1).toUpperCase() +
+//         word.slice(1, word.length).toLowerCase()
+//     )
+//     .join(" ");
 // };
 
-// console.log(getMass(characters));
+// console.log(properCase("arNav bhaTtarai"));
 
-// // Get the total number of characters in all the character names
-// const getNumberOfCharacters = (array) => {
-//   return array.reduce(
-//     (previousValue, currentValue) => previousValue + currentValue.name.length,
-//     0
-//   );
-// };
-// console.log(getNumberOfCharacters(characters));
+// 3. Get values of all characters using reduce method
+const characters = [
+  {
+    name: "Luke Skywalker",
+    height: "172",
+    mass: "77",
+    eye_color: "blue",
+    gender: "male",
+  },
+  {
+    name: "Darth Vader",
+    height: "202",
+    mass: "136",
+    eye_color: "yellow",
+    gender: "male",
+  },
+  {
+    name: "Leia Organa",
+    height: "150",
+    mass: "49",
+    eye_color: "brown",
+    gender: "female",
+  },
+  {
+    name: "Anakin Skywalker",
+    height: "188",
+    mass: "84",
+    eye_color: "blue",
+    gender: "male",
+  },
+];
+// Get total mass of all characters using reduce method
+const getMass = (arr) => {
+  return arr.reduce((prev, current) => prev + Number(current.mass), 0); //for first iteration , prev is 0 and for next iterations, prev is previously added values.
+};
+
+console.log(getMass(characters));
+
+// Get the total number of characters in all the character names
+const getNumberOfCharacters = (array) => {
+  return array.reduce(
+    (previousValue, currentValue) => previousValue + currentValue.name.length,
+    0
+  );
+};
+console.log(getNumberOfCharacters(characters));
 
